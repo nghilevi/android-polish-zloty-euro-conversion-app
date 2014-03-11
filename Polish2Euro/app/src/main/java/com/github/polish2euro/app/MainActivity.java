315d046354a357def53178d9d3f0ef53a74e459e
Nghi Le <vinhnghi223@gmail.com>
2014-03-11 13:54:38 +0100

    update 1

--- Polish2Euro/app/src/main/java/com/github/polish2euro/app/MainActivity.java
+++ Polish2Euro/app/src/main/java/com/github/polish2euro/app/MainActivity.java
@@ -4,16 +4,55 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity{
+    private static String url = "http://rate-exchange.appspot.com/currency?from=PLN&to=EUR";
+    private static final String PLN_TO_EUR_RATE = "rate";
+    Button convertButton = (Button) findViewById(R.id.convert);
+    EditText polishCurrency=(EditText)findViewById(R.id.polishCurrency);
+
+    int plnToEurRate;
+    int pln;
+    int result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        /*JSONParser jParser = new JSONParser();
+
+        try {
+            // get JSON Object (not JSON Array) data from URL
+            JSONObject jsonObject = jParser.getJSONFromUrl(url);
+            plnToEurRate = jsonObject.getInt(PLN_TO_EUR_RATE);
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }*/
+
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*if(polishCurrency != null) {
+                    pln = Integer.parseInt(polishCurrency.getText().toString());
+                } else {
+                    pln=0;
+                }*/
+                //result=pln*plnToEurRate;
+                result=0;
+               // ((TextView)findViewById(R.id.polishCurrency)).setText(result);
+            }
+        });
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,5 +73,4 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
